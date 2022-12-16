af3692b2978cb9b92d37a59bbf8a8a27af599ae0
Shairyar Baig <shairyarbaig@gmail.com>
2022-12-16 16:27:32 +0500

    Help Jan with instrumentation

---
+++ test.rb
@@ -0,0 +1,31 @@
+require 'appsignal'
+
+Appsignal.config = Appsignal::Config.new(
+  File.expand_path('../', __FILE__),          # Application root path
+  'development',                              # Application environment
+  :name => 'single-task',
+  :log_path => 'logs',
+  :lov_level => 'trace'                       # Optional configuration hash
+)
+
+
+Appsignal.start_logger
+Appsignal.start
+
+at_exit do
+  Appsignal.stop
+  sleep 5
+end
+
+class OffSiteBackupInstrumentation
+  # Omitted
+  def export_stats
+    Appsignal.increment_counter('off_site_backup.count', 1)
+    Appsignal.set_gauge('off_site_backup.snapshot_count', 1)
+    Appsignal.set_gauge('off_site_backup.total_size', 1)
+  end
+end
+
+Appsignal.monitor_transaction('perform_job.off_site_backup', class: 'OffSiteBackupInstrumentation', method: 'export_stats', metadata: { postgres_database: "DB URL GOES HERE" }) do
+  OffSiteBackupInstrumentation.new.export_stats
+end
