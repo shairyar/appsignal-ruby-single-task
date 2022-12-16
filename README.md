2b4ff0c79ae0bc659b5b44fdbec885e8c0ad5227
Shairyar Baig <shairyarbaig@gmail.com>
2022-12-16 11:20:22 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Integration ruby script with [AppSignal](https://docs.appsignal.com/ruby)
+
+## Setup
+
+```
+bundle install
+ruby app.rb
+```
---
+++ app.rb
@@ -0,0 +1,68 @@
+require "securerandom"
+# Setup AppSignal
+require "appsignal"
+require "appsignal/integrations/object"
+
+Appsignal.config = Appsignal::Config.new(
+  File.expand_path('../', __FILE__),          # Application root path
+  'development',                              # Application environment
+  :name => 'single-task',
+  :log_path => 'logs',
+  :lov_level => 'trace'                       # Optional configuration hash
+)
+
+
+Appsignal.start_logger
+Appsignal.start
+
+at_exit do
+  # Make sure all transactions are flushed to the Agent.
+  Appsignal.stop
+  sleep 5
+end
+
+class CustomError < StandardError; end
+
+# The app itself
+class Monitor
+  def perform(param1, options = {}, foo:, error:)
+    Appsignal.instrument("perform.do_stuff") do
+      puts "Monitor is doing stuff. Params: #{param1}, #{options}, #{foo}, #{error}"
+      sleep 1
+    end
+    Appsignal.increment_counter "test_counter", rand(1..3), "host" => "my-host"
+    Appsignal.set_gauge "test_gauge", rand(1..100), "host" => "my-host"
+    Appsignal.add_distribution_value "test_distribution", rand(1..100), "host" => "my-host"
+
+    raise CustomError, "Demo error" if error
+  end
+  appsignal_instrument_method :perform
+end
+
+20.times do |i|
+  # Test with monitor_transaction helper
+  begin
+    Appsignal.monitor_transaction("perform_job.monitor", :class => "MonitorTransaction", :method => "loop") do
+      Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+    end
+  rescue CustomError
+    # Do nothing, tracked by monitor_transaction
+  end
+
+  # Test using transaction class directly, like most AppSignal integrations do
+  begin
+    transaction = Appsignal::Transaction.create(
+      SecureRandom.uuid,
+      Appsignal::Transaction::HTTP_REQUEST,
+      Appsignal::Transaction::GenericRequest.new({})
+    )
+    Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+  rescue => error
+    transaction.set_error(error)
+  ensure
+    transaction.set_action_if_nil("MonitorCustomTransaction#loop")
+    Appsignal::Transaction.complete_current!
+  end
+
+  sleep 1
+end
